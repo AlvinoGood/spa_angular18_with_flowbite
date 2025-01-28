4a086bea19fedb1400ed248290e602df626fd33d
rtz <levitagoodlvns@gmail.com>
2025-01-28 11:11:28 -0500

    first umpload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./shared/components/nav-bar/nav-bar.component";
+import { FirstViewComponent } from "./sections/first-view/first-view.component";
+import { ServicesComponent } from "./sections/services/services.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FirstViewComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/sections/services-gallery/services-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-gallery',
+  imports: [],
+  templateUrl: './services-gallery.component.html',
+  styleUrl: './services-gallery.component.scss'
+})
+export class ServicesGalleryComponent {
+
+}
